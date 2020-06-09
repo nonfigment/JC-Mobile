8bcc16302635e3114ce98febace12d5cc3fecdb3
Dmitry T <tereshonok@protonmail.com>
2020-06-09 22:58:31 +0300

    Updated main JC logic

--- app/src/main/java/com/redshift/jumpcalc/Ship.java
+++ app/src/main/java/com/redshift/jumpcalc/Ship.java
@@ -2,9 +2,9 @@ package com.redshift.jumpcalc;
 
 public class Ship {
 
-    public String name;
-    public float range;
-    public int routeTime;
-    public boolean hasmodifiers;
+    String name;
+    float range;
+    int routeTime;
+    boolean hasmodifiers;
 
 }
